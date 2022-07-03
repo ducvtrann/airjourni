7f9be1f810c713ded15617d16c4a3315821008ff
Duc Tran <ducvtrann@gmail.com>
2022-07-03 18:26:52 -0400

    Add placeholder for the first deployment

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,24 @@
 import type { NextPage } from 'next';
-
+import Image from 'next/image';
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world</h1>;
+  return (
+    <div className="flex flex-col items-center w-screen h-screen bg-stone-800">
+      <h1 className="text-white">Hello World</h1>
+      <p className="mb-5 text-white">
+        Duc is building your next favorite app - AirJourni! He is learning
+        React, TailwindCSS, and a few more technologies to make your next trip
+        an amazing experience!
+      </p>
+      <div>
+        <Image
+          src="https://c.tenor.com/vjP1NdkBNQwAAAAC/duck-walk.gif"
+          alt="Walking duck"
+          width={1000}
+          height={1000}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
